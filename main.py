82797589541059350bea4d311b748fbb3b31637f
luoruiming <11621073@zju.edu.cn>
2019-11-02 11:27:49 +0800

    Implement DQN and train the model

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from unityagents import UnityEnvironment
 import numpy as np
+from collections import deque
+from dqn_agent import Agent
+import torch
+import matplotlib.pyplot as plt
+
 
 env = UnityEnvironment(file_name="Banana_Windows_x86_64/Banana.exe")
 
@@ -22,20 +27,50 @@ print('States look like:', state)
 state_size = len(state)
 print('States have length:', state_size)
 
-env_info = env.reset(train_mode=False)[brain_name]  # reset the environment
-state = env_info.vector_observations[0]  # get the current state
-score = 0  # initialize the score
-while True:
-    action = np.random.randint(action_size)  # select an action
-    env_info = env.step(action)[brain_name]  # send the action to the environment
-    next_state = env_info.vector_observations[0]  # get the next state
-    reward = env_info.rewards[0]  # get the reward
-    done = env_info.local_done[0]  # see if episode has finished
-    score += reward  # update the score
-    state = next_state  # roll over the state to next time step
-    if done:  # exit loop if episode finished
-        break
+agent = Agent(state_size=state_size, action_size=action_size, seed=0)
 
-print("Score: {}".format(score))
+def dqn(n_episodes=4000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.999):
+    scores = []
+    scores_window = deque(maxlen=100)
+    eps = eps_start
+    for i_episode in range(1, n_episodes + 1):
+        env_info = env.reset(train_mode=True)[brain_name]  # reset the environment
+        state = env_info.vector_observations[0]  # get the current state
+        score = 0
+        for t in range(max_t):
+            action = agent.act(state, eps)
+            if type(action) is np.int64:
+                action = action.astype(np.int32)
+            env_info = env.step(action)[brain_name]  # send the action to the environment
+            next_state = env_info.vector_observations[0]  # get the next state
+            reward = env_info.rewards[0]  # get the reward
+            done = env_info.local_done[0]  # see if episode has finished
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            score += reward
+            if done:
+                break
+        scores_window.append(score)
+        scores.append(score)
+        eps = max(eps_end, eps_decay * eps)
+        print('\rEpisode {}\tAverage Score:{:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+        if i_episode % 100 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        if np.mean(scores_window) >= 13.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode - 100,
+                                                                                         np.mean(scores_window)))
+            torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pth')
+            break
+    return scores
+
+scores = dqn()
+
+# plot the scores
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(np.arange(len(scores)), scores)
+plt.ylabel('Score')
+plt.xlabel('Episode #')
+plt.show()
 
 env.close()
